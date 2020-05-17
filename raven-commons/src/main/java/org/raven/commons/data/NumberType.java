c56bd8df525fa56314dc2419eb1fd451d56e918b
yi.liang <indifer@126.com>
2020-05-17 20:41:23 +0800

    add contract model

--- raven-commons/src/main/java/org/raven/commons/data/NumberType.java
+++ raven-commons/src/main/java/org/raven/commons/data/NumberType.java
@@ -33,9 +33,18 @@ public abstract class NumberType<V extends Number, T extends NumberType> impleme
 
     @Override
     public boolean equals(Object obj) {
-        if (obj != null && obj instanceof ValueType) {
+        if (obj == null) {
+            return false;
+        }
+
+        if (obj instanceof ValueType) {
             return value.equals(((ValueType) obj).getValue());
         }
+
+        if (obj instanceof Number) {
+            return obj.equals(value);
+        }
+
         return false;
     }
 
