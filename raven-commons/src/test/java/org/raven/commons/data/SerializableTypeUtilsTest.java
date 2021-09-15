340db2c2ccd17a75829f133e60ec33ad33c26c2f
yi.liang <indifer@126.com>
2021-09-15 16:01:53 +0800

    fixed bug for SerializableTypeUtils up to 3.1.1

--- raven-commons/src/test/java/org/raven/commons/data/SerializableTypeUtilsTest.java
+++ raven-commons/src/test/java/org/raven/commons/data/SerializableTypeUtilsTest.java
@@ -3,6 +3,10 @@ package org.raven.commons.data;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
 public class SerializableTypeUtilsTest {
 
     @Test
@@ -43,6 +47,39 @@ public class SerializableTypeUtilsTest {
         Assert.assertEquals(
                 SerializableTypeUtils.valueOf(OperationEnum.class, "nin").getValue(), OperationEnum.NIN.getValue()
         );
+
+    }
+
+
+    @Test
+    public void multiThreadTest() throws Exception {
+
+        int seed = 100;
+        List<Thread> futures = new ArrayList<>(seed);
+
+        for (int i = 0; i < seed; i++) {
+
+            Thread thread = new Thread(() -> {
+
+                DBType dbType = SerializableTypeUtils.valueOf(DBType.class, 1);
+                System.out.println(dbType);
+
+            });
+
+            futures.add(thread);
+        }
+
+        for (Thread future : futures) {
+            future.start();
+        }
+
+        for (Thread future : futures) {
+            future.join();
+
+        }
+
+        System.out.println("ggg");
+
 
     }
 
